a4ceb9cb1d59d01d6ef2f6f7dfef82863625b2e6
Adolfo G <93852030+GJZ26@users.noreply.github.com>
2023-08-02 01:35:26 -0600

    Revert "Bugfix 🐞: Autoincremental keys increases even if registration is not completed successfully"

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UsuarioController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,9 +17,3 @@ use App\Http\Controllers\UsuarioController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('domicilioResidencias',DomicilioResidenciaController::class);
-Route::post("/login",[UsuarioController::class, 'login']);
-Route::post("/register",[UsuarioController::class, 'register']);
-
-Route::any("{any}",function(){return response()->json(["message"=>"Route not found"])->setStatusCode(404);});
